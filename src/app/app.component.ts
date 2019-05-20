ab9f99e5dc9c008725d8a3ddee6fbb42259987f8
Monica Beauchamp <monica.beauchamp@fmr.com>
2019-05-20 14:36:43 -0600

    Basic routing set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  showRecipes = true;
-  showList = false;
-
-  updateView(event) {
-    const view = event.chosenView;
-    if (view === 'show-recipes') {
-      this.showRecipes = true;
-      this.showList = false;
-    } else if (view === 'show-list') {
-      this.showRecipes = false;
-      this.showList = true;
-    }
-  }
-}
+export class AppComponent {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-  @Output() updateView = new EventEmitter<{
-    chosenView: string
-  }>();
-
-  onUpdateView(event) {
-    this.updateView.emit({chosenView: event.target.className});
-  }
-}
+export class HeaderComponent {}
